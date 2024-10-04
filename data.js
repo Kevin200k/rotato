174015c3c40c78494d0440016d6db817821d15c7
inxxetheman <ikenwetobe@gmail.com>
2024-10-04 09:45:24 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,65 @@
+{
+    "matches": [
+      {
+        "date": "2024-10-05",
+        "time": "12:30",
+        "home_team": "Crystal Palace",
+        "away_team": "Liverpool"
+      },
+      {
+        "date": "2024-10-05",
+        "time": "15:00",
+        "home_team": "Arsenal",
+        "away_team": "Southampton"
+      },
+      {
+        "date": "2024-10-05",
+        "time": "15:00",
+        "home_team": "Brentford",
+        "away_team": "Wolverhampton Wanderers"
+      },
+      {
+        "date": "2024-10-05",
+        "time": "15:00",
+        "home_team": "Leicester City",
+        "away_team": "AFC Bournemouth"
+      },
+      {
+        "date": "2024-10-05",
+        "time": "15:00",
+        "home_team": "Manchester City",
+        "away_team": "Fulham"
+      },
+      {
+        "date": "2024-10-05",
+        "time": "15:00",
+        "home_team": "West Ham United",
+        "away_team": "Ipswich Town"
+      },
+      {
+        "date": "2024-10-05",
+        "time": "17:30",
+        "home_team": "Everton",
+        "away_team": "Newcastle United"
+      },
+      {
+        "date": "2024-10-06",
+        "time": "14:00",
+        "home_team": "Aston Villa",
+        "away_team": "Manchester United"
+      },
+      {
+        "date": "2024-10-06",
+        "time": "14:00",
+        "home_team": "Chelsea",
+        "away_team": "Nottingham Forest"
+      },
+      {
+        "date": "2024-10-06",
+        "time": "16:30",
+        "home_team": "Brighton & Hove Albion",
+        "away_team": "Tottenham Hotspur"
+      }
+    ]
+  }
+  
